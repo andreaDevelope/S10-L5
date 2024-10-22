63e6a288f22e08ac1626230b5c14f52f3112b311
andreaDevelope <andrea_develope@outlook.it>
2024-10-22 21:40:01 +0200

    fixed-users-page-&-homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { iTodo } from '../../moduls/i-todo';
 import { TodoServiceService } from '../../services/todo-service.service';
 import { SearchServiceService } from '../../services/search-service.service';
+import { UserServiceService } from '../../services/user-service.service';
+import { iUsers } from '../../moduls/i-users';
 
 @Component({
   selector: 'app-home',
@@ -11,18 +13,22 @@ import { SearchServiceService } from '../../services/search-service.service';
 export class HomeComponent {
   todoArr: iTodo[] = [];
   filteredTodos: iTodo[] = [];
+  ibridUserArr: iUsers[] = [];
 
   constructor(
     private todoServ: TodoServiceService,
-    private searchServ: SearchServiceService
+    private searchServ: SearchServiceService,
+    private userServ: UserServiceService
   ) {}
 
   ngOnInit() {
     this.todoServ.todoArr$.subscribe((todos) => {
       this.todoArr = todos;
     });
+    this.ibridUserArr = this.userServ.getUserWithTodo(this.todoArr);
 
     this.todoArr = this.todoServ.todoArr;
+    console.log(this.ibridUserArr);
 
     this.searchServ.getSearchTerm().subscribe((term: string) => {
       if (term.trim() === '') {
